312942074ff522a9e324b101a55417e7a582ba7a
Jon Borrelli <borrejj@gmail.com>
2018-09-21 15:38:59 -0400

    new sim tests

--- rscripts/simulation.R
+++ rscripts/simulation.R
@@ -14,12 +14,12 @@ source("rscripts/plotting.R")
 # Set up the lake
 
 # Horizontal dimension
-xdim <- 20
+xdim <- 40
 # Vertical dimension
-ydim <- 20
+ydim <- 40
 
 # Number of simulation timesteps (days)
-timesteps <- 300
+timesteps <- 100
 
 
 # set up array to store biomass of prey
@@ -30,15 +30,16 @@ lake.y <- array(0, dim = c(ydim, xdim, timesteps))
 lake.z <- array(0, dim = c(ydim, xdim, timesteps))
 # set up lake phosphorus concentration
 # gradient
-lake.P <- matrix(sort(runif(xdim*ydim, .03, .05)), nrow = ydim, ncol = xdim)
-lake.P <- lake.P[sample(1:nrow(lake.P)),]
+lake.P <- array(0, dim = c(ydim, xdim, timesteps))
+lake.P[,,1] <- matrix(sort(runif(xdim*ydim, .08, .08)), nrow = ydim, ncol = xdim)
+lake.P[,,1] <- lake.P[sample(1:nrow(lake.P)),,1]
 #cols <- rev(sort(sample(1:ncol(lake.P), ncol(lake.P)/2)))
 #lake.P <- (lake.P[,c(cols, (1:nrow(lake.P))[-cols])])
 # initial biomass for both species
 lake.x[,,1] <- matrix(runif(ydim * xdim), ydim, xdim)
 lake.y[,,1] <- matrix(runif(ydim * xdim), ydim, xdim)
 #lake.z[,,1] <- matrix(runif(ydim * xdim), ydim, xdim)
-lake.z[,,1] <- matrix((lake.P - par.P14fixed$theta * lake.y[,,1])/lake.x[,,1])
+lake.z[,,1] <- matrix((lake.P[,,1] - par.P14fixed$theta * lake.y[,,1])/lake.x[,,1])
 
 ###############################
 # Simulate dynamics
@@ -54,7 +55,7 @@ for(t in 1:(timesteps - 1)){
       lake.x[,,t][lake.x[,,t] == 0] <- 10^-4
       
       
-      par.P14fixed$P <- lake.P[i,j]
+      par.P14fixed$P <- lake.P[i,j,t]
       state = c(x = lake.x[i,j,t], y = lake.y[i,j,t], Q = lake.z[i,j,t])
       
       out <- ode(state, 1:2, P14_fixed, par.P14fixed)
@@ -70,11 +71,15 @@ for(t in 1:(timesteps - 1)){
   }
   errors.t[t] <- any(is.na(errors.i))
   
+  lake.x[,,t+1][lake.x[,,t+1] < 0] <- 0
+  lake.y[,,t+1][lake.y[,,t+1] < 0] <- 0
 
   ## Zoop Migration
-  lake.y[,,t+1] <- migration(lake.y[,,t+1], lake.x[,,t+1], lake.z[,,t+1],
-                             max.move = 1, prop.migrant = .1, method = "density")
-  
+  mig <- migration(lake.y[,,t+1], lake.x[,,t+1], lake.z[,,t+1], lake.P[,,t], 
+                   par.P14fixed$theta,
+                             max.move = 1, prop.migrant = .1, method = "quota")
+  lake.y[,,t+1] <- mig[[1]]
+  lake.P[,,t+1] <- mig[[2]]
   
   if(((t + 1) %% 10) == 0){cat("Day ", t+1, "of ", timesteps, "simulated\n")}
 }
@@ -83,35 +88,60 @@ t1-t0
 
 
 #run1 <- list(lake.x, lake.y, lake.z, lake.P)
-run2 <- list(lake.x, lake.y, lake.z, lake.P)
+#run2 <- list(lake.x, lake.y, lake.z, lake.P)
 #run3 <- list(lake.x, lake.y, lake.z, lake.P)
 #run4 <- list(lake.x, lake.y, lake.z, lake.P)
+
+#run1.1 <- list(lake.x, lake.y, lake.z, lake.P)
+#run2.1 <- list(lake.x, lake.y, lake.z, lake.P)
+#run3.1 <- list(lake.x, lake.y, lake.z, lake.P)
+
+#run1.2 <- list(lake.x, lake.y, lake.z, lake.P)
+run2.2 <- list(lake.x, lake.y, lake.z, lake.P)
+#run3.2 <- list(lake.x, lake.y, lake.z, lake.P)
+
+
 ###############################
 # Visualize results
-test <- run2
+test <- run1.2
 threePlot(3, test[[3]], test[[2]], test[[1]])
-threePlot(3, lake.z, lake.y, lake.x)
+threePlot(5, lake.z[,,], lake.y[,,], lake.x[,,])
 
 #par(mfrow = c(2,1), mar = c(1,2,1,.2))
 
-pheatmap(apply(lake.x[,,250], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = 5, cellheight = 5)
-pheatmap(apply(lake.y[,,251], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = 5, cellheight = 5)
-pheatmap(apply(lake.z[,,250], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = 5, cellheight = 5)
+cell <- 5
+iter <- 90
+pheatmap(apply(lake.x[,,iter], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = cell, cellheight = cell)
+pheatmap(apply(lake.y[,,iter], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = cell, cellheight = cell)
+pheatmap(apply(lake.z[,,iter], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = cell, cellheight = cell)
 
+pheatmap(apply(lake.P[,,iter], c(1,2), mean), cluster_rows = F, cluster_cols = F, cellwidth = cell, cellheight = cell)
 
-pheatmap(lake.P, cluster_rows = F, cluster_cols = F, cellwidth = 5, cellheight = 5)
+#pheatmap(lake.P, cluster_rows = F, cluster_cols = F, cellwidth = 5, cellheight = 5)
 
-saveRDS(list(lake.x, lake.y, lake.z, lake.P), "sides_gradientP_run_100.rds")
+#saveRDS(list(lake.x, lake.y, lake.z, lake.P), "sides_gradientP_run_100.rds")
 #run <- readRDS("gradientP_run_100.rds")
 
 lake.y[1,1,]
 
-all2 <- cbind(apply(lake.x, 3, sum), apply(lake.y, 3, sum))
+
+all2 <- cbind(apply(run1[[1]], 3, mean), apply(run1[[2]], 3, mean))
 matplot(all2, typ = "l")
-matplot(all)
+all2 <- cbind(apply(run2[[1]], 3, mean), apply(run2[[2]], 3, mean))
+matplot(all2, typ = "l")
+all2 <- cbind(apply(run3[[1]], 3, mean), apply(run3[[2]], 3, mean))
+matplot(all2, typ = "l")
+
+all2 <- cbind(apply(run1.1[[1]], 3, mean), apply(run1.1[[2]], 3, mean))
+matplot(all2, typ = "l")
+all2 <- cbind(apply(run2.1[[1]], 3, mean), apply(run2.1[[2]], 3, mean))
+matplot(all2, typ = "l")
+all2 <- cbind(apply(run3.1[[1]], 3, mean), apply(run3.1[[2]], 3, mean))
+matplot(all2, typ = "l")
+#matplot(all2)
 
 spatdynplot(lake.x, t = timesteps, interval = .2, name = "xlake.html")
-spatdynplot(run[[2]], t = timesteps, interval = .2, name = "ylake.html")
+spatdynplot(lake.y, t = timesteps, interval = .2, name = "ylake.html")
 dev.off()
 
 matplot(t(apply(lake.x, 3, colMeans)), typ = "l")
